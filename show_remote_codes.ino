f13e152a77d517d9c2f85827ed71444cd86aea64
Rodgers <59391568+butasi@users.noreply.github.com>
2022-08-13 21:50:26 +0300

    Update show_remote_codes.ino

--- show_remote_codes.ino
+++ show_remote_codes.ino
@@ -1,18 +1,18 @@
 #include <IRremote.h>
 
-const int RECV_PIN = 7;
+const int RECV_PIN = 7; //connect receiver pin
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600); //start serial
   irrecv.enableIRIn();
   irrecv.blink13(true);
 }
 
 void loop(){
   if (irrecv.decode(&results)){
-        Serial.println(results.value, HEX);
+        Serial.println(results.value, HEX);//print pressed button on serial monitor
         irrecv.resume();
   }
 }
